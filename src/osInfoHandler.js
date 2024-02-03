fd4cd08e9bafc31a51a3ca99786d7c3d6443d4bc
ST359 <135271126+ST359@users.noreply.github.com>
2024-02-04 02:02:47 +0300

    feat: implement os info handler

---
+++ src/osInfoHandler.js
@@ -0,0 +1,28 @@
+import os from "node:os";
+
+export const osInfoHandler = (arg) => {
+    switch (arg) {
+        case "--EOL":
+            console.log(os.EOL);
+            break;
+        case "--cpus":
+            let cpusArr = os.cpus();
+            cpusArr.forEach((cpu) => cpu.speed = `${cpu.speed/1000} GHz`);
+            console.log(`\nOverall amount of CPUS: ${cpusArr.length}`);
+            console.table(cpusArr, ["model", "speed"]);
+            break;
+        case "--homedir":
+            console.log(os.homedir());
+            break;
+        case "--username":
+            console.log(os.userInfo({ encoding: "UTF-8" }).username);
+            break;
+        case "--architecture":
+            console.log(os.arch());
+            break;
+        default:
+            console.log('Unknown os operation argument');
+            break;
+    }
+};
+
